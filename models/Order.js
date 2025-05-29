5ebafc9c478af1b2c12e5ae5e19b965285491abc
Dayana-LmP <lamaspinedodayana@gmail.com>
2025-05-28 22:06:30 -0600

    Update models

--- models/Order.js
+++ models/Order.js
@@ -1,1 +1,36 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+  nombre: {
+    type: String,
+    required: true
+  },
+  drink: {
+    type: String,
+    required: true,
+    enum: ['americano', 'cappuccino', 'espresso', 'latte']
+  },
+  ounce: {
+    type: Number,
+    required: true,
+    enum: [8, 12, 16]
+  },
+  shots: {
+    type: Number,
+    default: 1,
+    min: 1,
+    max: 5
+  },
+  precio: {
+    type: Number,
+    required: true
+  },
+  estatus: {
+    type: String,
+    enum: ['Pendiente', 'Entregado'],
+    default: 'Pendiente'
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Order', orderSchema);
 
